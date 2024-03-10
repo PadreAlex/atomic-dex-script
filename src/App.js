28ed461b21dd43bda534e3910a0ebbe663c64b48
Maksim Eremin <meremin@sfxdx.com>
2024-03-10 14:05:40 +0500

    link upd

--- src/App.js
+++ src/App.js
@@ -6,19 +6,31 @@ import { Routes, Route } from "react-router-dom";
 import { useConnect, useAccount, useDisconnect } from "wagmi";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 import { useState, useEffect } from "react";
-import  "./test.js";
 function App() {
   useEffect(() => {
+    var loadScript = function (src) {
+      var tag = document.createElement("script");
+      tag.async = false;
+      tag.src = src;
+      tag.id = "adScript";
+      var body = document.getElementsByTagName("body")[0];
+      body.appendChild(tag);
+    };
 
-   window.getitAdScript.renderAdPlugin(
-      {
-        apiKey:
-          "DhoFm82C6XN2bbs3tnuGTIVF3IHedbNhYl5dqoCZVrrKajMePFbpLUZtd4LO17xbh36NjLbNZynbvri3OzOwiMfwJIjVH20Le2QdhS71QEpxJ71Hj7zZf1M1r0qbaZCx",
-        walletConnected: "",
-        slotId: "1",
-      },
-      "adContainer"
+    loadScript(
+      "https://cdn.jsdelivr.net/gh/Getit-Tech/getit-script@main/getit-script-experimental.js"
     );
+    document.getElementById("adScript").addEventListener("load", () => {
+      window.getitAdScript.renderAdPlugin(
+        {
+          apiKey:
+            "DhoFm82C6XN2bbs3tnuGTIVF3IHedbNhYl5dqoCZVrrKajMePFbpLUZtd4LO17xbh36NjLbNZynbvri3OzOwiMfwJIjVH20Le2QdhS71QEpxJ71Hj7zZf1M1r0qbaZCx",
+          walletConnected: "",
+          slotId: "1",
+        },
+        "adContainer"
+      );
+    });
   }, []);
 
   const { address, isConnected } = useAccount();
@@ -35,7 +47,7 @@ function App() {
         isConnected={isConnected}
         address={address}
       />
-      <div id='adContainer'></div>
+      <div id="adContainer"></div>
       <div className="mainWindow">
         <Swap isConnected={isConnected} address={address} />
       </div>
